bb6a95b239ef35fc84df6429513beb2ee220750e
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-25 12:37:17 +0300

    app entry point

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
+import { StoreProvider } from '@/app/providers/StoreProvider';
+import { App } from '@/app/App';
+import '@/app/styles/index.scss';
+import '@/shared/config/i18n/i18n';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+    <BrowserRouter>
+        <StoreProvider>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </StoreProvider>
+    </BrowserRouter>,
+);
+export { Theme } from '@/shared/const/theme';
