3ccff6cbad9db384c9cd431a0e8129c362824335
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-25 12:27:31 +0300

    jest config

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+
+export default {
+    globals: {
+        __IS_DEV__: true,
+        __API__: '',
+        __PROJECT__: 'jest',
+    },
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+    moduleDirectories: ['node_modules'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    rootDir: '../../',
+    testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    modulePaths: ['<rootDir>src'],
+    moduleNameMapper: {
+        '\\.s?css$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'JestEmptyComponent.tsx'),
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    reporters: [
+        'default',
+        [
+            'jest-html-reporters',
+            {
+                publicPath: '<rootDir>/reports/unit',
+                filename: 'report.html',
+                openReport: false,
+                inlineSource: true,
+            },
+        ],
+    ],
+};
