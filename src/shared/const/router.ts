2e087fb6b7a0d6823d640ecb5c842516e49acae1
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-25 12:47:01 +0300

    constants

---
+++ src/shared/const/router.ts
@@ -0,0 +1,9 @@
+export const getRouteMain = () => '/';
+export const getRouteAbout = () => '/about';
+export const getRouteProfile = (id: string) => `/profile/${id}`;
+export const getRouteArticles = () => '/articles';
+export const getRouteArticleDetails = (id: string) => `/articles/${id}`;
+export const getRouteArticleCreate = () => '/articles/new';
+export const getRouteArticleEdit = (id: string) => `/articles/${id}/edit`;
+export const getRouteAdminPanel = () => '/admin';
+export const getRouteForbidden = () => '/forbidden';
